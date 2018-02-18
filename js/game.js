d5bbc506cc24c89d6b97a452ce015f3288e22c2b
Maryja Peredryj <chysapko@gmail.com>
2018-02-18 18:32:57 +0300

    Added comments, delete unnecessary files

--- js/game.js
+++ js/game.js
@@ -1,3 +1,12 @@
+/*
+* Игра в города.
+* Игра использует Geonames API. Каждый ввод города посылает запрос к Geonames и получает результат в виде
+* списка городов, которые могут быть подходящими. Т.е., города получаются по одному в ходе игры. Это позволяет
+* называть в игре любой город мира, что было бы невозможно при полученном или заданном заранее списке городов,
+* потому что они заведомо менее полные.
+*
+* */
+
 (function () {
     ymaps.ready(init);
 
@@ -29,7 +38,7 @@
 
         if (firstPlayer.player === 0) {
             currentLastChar = randomLetter();
-            setTimeout(function () {
+            setTimeout(function () { // отсрочка вызова на 1,5 сек - имитация того, что компьютер «думает»
                 compInput(currentLastChar)
             }, 1500);
         } else {
@@ -45,6 +54,8 @@
         sendBtn.addEventListener('click', function (e) {
             e.preventDefault();
             let value = townInput.value;
+
+            // валидация поля ввода города
             if (value === '') {
                 showError("Поле не должно быть пустым");
             } else if (isNamed(value)) {
@@ -54,17 +65,20 @@
             } else {
                 userInput(value);
             }
+
         });
 
         document.getElementById('concede').addEventListener('click', function (e) {
             e.preventDefault();
             showResults('comp');
-            // showResults('user');
         });
 
-        // console.log(cities);
-        // console.log(getLastChar('Неаполь'));
-
+        /**
+         * Функция выбирает, кто ходит первым: компьютер или юзер.
+         *
+         * @returns {number} player 0 - компьютер, 1 - юзер
+         * @returns {string} phrase информационная фраза, соответствующая выбранному игроку
+         */
         function drawPlayer() {
             let phrase = ['первым ходит компьютер', 'первый ход за вами'],
                 player = Math.round(Math.random());
@@ -75,16 +89,27 @@
             };
         }
 
+
+        /**
+         * Функция обработки того, что ввёл пользователь.
+         * Функция отправлет запрос к Geonames API и получает список городов, отвечающих тому, что ввёл юзер.
+         * Отфильтровывает и выбирает из них тот, который скорее всего ввёл пользователь. Добавляет город на карту.
+         * Фильтрация нужна, потому что Geonames на запрос "Минск" может вернуть геообъекты с названиями и "Минск",
+         * и "Horad Minsk" и т.п.
+         *
+         * @param {string} value пользователем название города
+         */
+
         function userInput(value) {
             const url = 'https://secure.geonames.org/searchJSON?name=' + value + '&cities=cities15000&lang=ru&style=MEDIUM&maxRows=20&fclass=P&username=mankutila';
             let place = {};
             fetch(url)
                 .then((resp) => resp.json())
                 .then(function (data) {
-                    console.log(data);
+
                     if (data.totalResultsCount !== 0) {
                         let i = 0;
-                        while (!/^[А-ЯЁ\s-]*$/i.test(data.geonames[i].name)) {
+                        while (!/^[А-ЯЁ\s-]*$/i.test(data.geonames[i].name)) { //выбрать только кириллические названия
                             if (data.geonames.length - 1 === i) {
                                 document.getElementById('error').innerHTML = "Не могу найти город с таким названием";
                                 return false;
@@ -99,15 +124,15 @@
                             'owner': 'user'
                         });
                         currentLastChar = getLastChar(place.name);
-                        showResult('Текущая буква: ' + currentLastChar);
-                        showResult(cities[cities.length - 1].name + ' (' + cities[cities.length - 1].country + ')');
+                        showInfo('Текущая буква: ' + currentLastChar);
+                        showInfo(cities[cities.length - 1].name + ' (' + cities[cities.length - 1].country + ')');
                         userCollection.add(new ymaps.Placemark(cities[cities.length - 1].coord, {
                             balloonContent: '<b>' + cities[cities.length - 1].name + '</b><br> Назвал пользователь'
                         }));
                         townInput.value = '';
                         townInput.disabled = true;
                         sendBtn.disabled = true;
-                        setTimeout(function () {
+                        setTimeout(function () { // отсрочка вызова на 1,5 сек - имитация того, что компьютер «думает»
                             compInput(currentLastChar)
                         }, 1500);
                     } else {
@@ -120,13 +145,21 @@
                 });
         }
 
+        /**
+         * Функция хода компьютера.
+         * Функция запрашивает у Geonames API 50 городов на текущую букву, а затем выбирает из них случайный,
+         * отфильтровывая ненужные, и выводит. Добавляет выбранный город на карту
+         * Фильтрация нужна, потому что Geonames на запрос, например, на букву "г" может вернуть геообъекты с названиями и "Гродно",
+         * и "Горад Гродна", и с латиницей в названии и т.п.
+         *
+         * @param {string} firstChar буква, на которую компьютер должен назвать город
+         */
         function compInput(firstChar) {
             const url = 'https://secure.geonames.org/searchJSON?name_startsWith=' + firstChar + '&orderby=population&cities=cities15000&lang=ru&style=MEDIUM&maxRows=50&fclass=P&username=mankutila';
             let place = {}, index;
             fetch(url)
                 .then((resp) => resp.json())
                 .then(function (data) {
-                    console.log(data);
                     let dataArray = data.geonames;
 
                     index = Math.round(Math.random() * 50); //мы запрашиваем всегда только 50 городов, поэтому выбираем случайный из 50
@@ -151,8 +184,8 @@
                         balloonContent: '<b>' + cities[cities.length - 1].name + '</b><br> Назвал компьютер'
                     }));
                     currentLastChar = getLastChar(place.name);
-                    showResult('Текущая буква: ' + currentLastChar);
-                    showResult(cities[cities.length - 1].name + ' (' + cities[cities.length - 1].country + ')');
+                    showInfo('Текущая буква: ' + currentLastChar);
+                    showInfo(cities[cities.length - 1].name + ' (' + cities[cities.length - 1].country + ')');
                     townInput.disabled = false;
                     sendBtn.disabled = false;
 
@@ -162,15 +195,27 @@
                 });
         }
 
-        function isNamed(city) {
+        /**
+         * Функция проверяет, назывался ли город или нет
+         *
+         * @param {string} town название города
+         * @returns {boolean} true - назывался, false - не назывался
+         */
+        function isNamed(town) {
             for (let i = 0; i < cities.length; i++) {
-                if (city.toLowerCase() === cities[i].name.toLowerCase()) {
+                if (town.toLowerCase() === cities[i].name.toLowerCase()) {
                     return true;
                 }
             }
             return false;
         }
 
+        /**
+         * Функция получает последнюю букву слова с учётом правил игры в города
+         *
+         * @param {string} word название города
+         * @returns {string} char последняя буква названия города, отвечающая правилам игры
+         */
         function getLastChar(word) {
             let char = word.slice(-1);
             if (char === 'ь' || char === 'ъ' || char === 'ы' || char === 'й') {
@@ -179,17 +224,32 @@
             return char;
         }
 
+        /**
+         * Функция получает случайную букву алфавита (кроме ь, ъ, ы и й)
+         *
+         * @returns {string} случайная буква
+         */
         function randomLetter() {
             const alphabet = 'абвгдеёжзиклмнопрстуфхцчшщэюя';
             return alphabet[Math.round(Math.random() * (alphabet.length - 1))];
         }
 
-        function showResult(txt) {
+        /**
+         * Функция выводит информационные сообщения в блок с текущими результатами игры
+         *
+         * @param {string} txt Текст, который нужно вывести
+         */
+        function showInfo(txt) {
             let info = document.createElement('p');
             info.innerHTML = txt;
             document.getElementsByClassName('game__result')[0].prepend(info);
         }
 
+        /**
+         * Функция выводит информационные сообщения после окончания игры
+         *
+         * @param {string} winner Имя игрока, который выиграл. 'comp' - компьютер, 'user' - пользователь
+         */
         function showResults(winner) {
             document.getElementsByClassName('game')[0].style.display = 'none';
             finish.style.display = 'flex';
@@ -214,19 +274,25 @@
                     }
                 }
             }
-
         }
 
+        /**
+         * Функция показывает ошибку валидации поля ввода города
+         *
+         * @param {string} txt Текст ошибки
+         */
         function showError(txt) {
             errorBlock.innerHTML = txt;
             townInput.value = '';
             townInput.classList.add('txt-input--invalid');
         }
 
+        /**
+         * Функция скрывает ошибку валидации поля ввода города
+         */
         function hideError() {
             errorBlock.innerHTML = '';
             townInput.classList.remove('txt-input--invalid');
-
         }
 
     }
